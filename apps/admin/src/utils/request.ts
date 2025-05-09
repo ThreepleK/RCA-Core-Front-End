c2ad7eb1d7bf37d8d72a2fbfc0503a4215f03751
TK <kg_kang@bistelligence.ai>
2025-05-09 14:58:01 +0900

    # 공용   - request 요청 함수 기능 업데이트     · data 항목 추가     · param, data 내용 데이터 인코딩 처리

--- apps/admin/src/utils/request.ts
+++ apps/admin/src/utils/request.ts
@@ -1,17 +1,22 @@
 import axios from 'axios'
 import { useUserStore } from '@repo/shared-state'
 
-export async function request(
-    type: 'get'|'post',     // 요청 타입
-    url: string,            // 요청 주소
-    params: any = {}        // 요청 데이터
-) {
+export async function request({ type, url, params={}, datas={} }: {
+    type: 'get'|'post';     // 요청 타입
+    url: string;            // 요청 주소
+    params: any;            // 요청 데이터 (url)
+    datas: any;             // 요청 데이터 (body)
+}) {
     try {
+        // 인증토큰 가져오기
         const { token } = useUserStore.getState();
+
+        // 요청
         const res = await axios({
             method: type,
             url,
-            params,
+            params: dataEncode(params),
+            data: dataEncode(datas),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer '+ token,
@@ -31,4 +36,52 @@ export async function request(
         const errMsg = err?.response?.statusText ?? 'request failed.';
         return {isErr: true, msg: errMsg, res: null};
     }
+}
+
+/**
+ * 데이터 인코딩 처리
+ */
+function dataEncode(data: any) {
+    // 데이터가 없을 경우
+    if( !data ){ return {}; }
+
+    // 데이터 타입 체크 (number, string, array, object)
+    const typeChk = (item: any) => {
+        if( Array.isArray(item) ){ return 'array'; }
+        return typeof item;
+    }
+
+    // 숫자 인코딩
+    const encodeNumber = (data: number) => data;
+    // 문자열 인코딩
+    const encodeString = (data: string) => encodeURIComponent(data);
+
+    // 반복
+    const loop = (items: any) => {
+        let result: any;
+        const t = typeChk(items);
+
+        if( t === 'array' ){
+            result = items.map((item: any) => {
+                return loop(item);
+            });
+        }
+        else if( t === 'object' ){
+            result = {};
+            for( const key in items ){
+                const item = items[key];
+                result[key] = loop(item);
+            }
+        }
+        else if( t === 'number' ){
+            result = encodeNumber(items);
+        }
+        else if( t === 'string' ){
+            result = encodeString(items);
+        }
+        
+        return result;
+    }
+
+    return loop(data);
 }
