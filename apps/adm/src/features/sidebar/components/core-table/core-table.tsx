e92c5e47a97f2045952e8e22a9e9ac6aea814eb5
yunny <sypark@bistelligence.ai>
2025-06-09 17:28:26 +0900

    # admin > sidebar - refactoring.........

---
+++ apps/adm/src/features/sidebar/components/core-table/core-table.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState, type ReactElement } from "react";
+import style from "./core-table.module.css";
+import { UI_Input, UI_Switch } from "@/compos/ui";
+import { useLocalSendEvent } from "../../stores";
+import { UI_Table } from "@/compos/ui/ui-table";
+import type { IFetchSidebarMenuItem } from "../../models";
+
+export function CoreTable({
+  dataSource,
+}: {
+  dataSource: IFetchSidebarMenuItem[];
+}): ReactElement {
+  const [data, setData] = useState<IFetchSidebarMenuItem[]>([]);
+
+  const { eKey, eVal, clean } = useLocalSendEvent.getState();
+  console.log('eVal', eVal)
+  console.log('eKey', eKey)
+
+  useEffect(() => {
+    if (!dataSource) return;
+
+    setData(dataSource);
+  }, [dataSource]);
+
+  const handleToggleChange = (id: string, checked: boolean) => {
+    setData((prev) =>
+      prev.map((row) =>
+        row.id === id ? { ...row, isVisible: checked, itemType: "update" } : row
+      )
+    );
+  };
+
+  const handleDisplayNameChange = (id: string, value: string) => {
+    setData((prev) =>
+      prev.map((row) =>
+        row.id === id ? { ...row, displayName: value, itemType: "update" } : row
+      )
+    );
+  };
+
+  const columns: any = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Display Name",
+      dataIndex: "displayName",
+      render: (text, record) => {
+        return text === "" ? (
+          <UI_Input
+            value={text}
+            onChange={(e) => {
+              handleDisplayNameChange(record.id, text);
+            }}
+          />
+        ) : (
+          text
+        );
+      },
+    },
+    {
+      title: "Display",
+      dataIndex: "isVisible",
+      render: (text, record) => (
+        <UI_Switch
+          checked={text}
+          onChange={(checked) => handleToggleChange(record.id, checked)}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <UI_Table
+      className={style.table}
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
+  );
+}
+
+export default CoreTable;
