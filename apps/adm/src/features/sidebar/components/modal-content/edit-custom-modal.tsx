de99d9f3283a8543416344d54f300d3591cf2835
yunny <sypark@bistelligence.ai>
2025-06-11 15:58:46 +0900

    # admin > sidebar - update modal content

---
+++ apps/adm/src/features/sidebar/components/modal-content/edit-custom-modal.tsx
@@ -0,0 +1,67 @@
+import { UI_Flex, UI_TextInput } from "@/compos/ui";
+import { UI_Typography } from "@/compos/ui/ui-typography";
+import { cloneDeep } from "lodash";
+import { useEffect, useState } from "react";
+
+export function EditCustomModal({
+  type,
+  row,
+  onSetData,
+  errMsg,
+  errCode,
+}: {
+  type: "new" | "mod";
+  row: any;
+  onSetData?: (value: any, key: string) => void;
+  errMsg?: string;
+  errCode?: string;
+}) {
+  const [r, setRow] = useState([]);
+
+  //* 초기 설정
+  useEffect(() => { 
+    if(!row) return;
+    const cloneData = cloneDeep(row);
+    setRow(cloneData)
+  }, [row]);
+
+  //* 데이터 설정
+  const onData = (value: any, key: string) => {
+    // 폼 데이터 재설정
+    setRow((old) => {
+      console.log('old', old)
+      old[key] = value;
+      console.log('value', value)
+      return { ...old };
+    });
+
+    // 변경 데이터 전달
+    // onSetData(value, key);
+  };
+
+  return (
+    <UI_Flex vertical gap={16}>
+      <div>
+        <UI_TextInput
+          label="Menu display name"
+          value={row.displayName}
+          error={errCode === "displayname" && errMsg}
+          required
+          onChange={(e) => onData(e.currentTarget.value, "displayName")}
+        />
+        </div>
+
+        <div>
+        <UI_TextInput
+          label="Redirect URL"
+          value={row.url}
+          error={errCode === "url" && errMsg}
+          required
+          onChange={(e) => onData(e.currentTarget.value, "url")}
+        />
+      </div>
+    </UI_Flex>
+  );
+}
+
+export default EditCustomModal;
